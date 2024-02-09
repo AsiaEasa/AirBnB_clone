d83ec21050c0d682102196b00caf3c0bda49ee48
AsiaEasa <asiaeasa89@gmail.com>
2024-02-09 05:17:48 -0800

    done

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,103 +1,103 @@
 #!/usr/bin/python3
-"""unittests for models/state.py.
+"""unittests for models/amenity.py.
 """
+
 import os
 import models
 import unittest
 from datetime import datetime
-from time import sleep
-from models.state import State
+from models.amenity import Amenity
 from models import storage
 
 
-class TestState(unittest.TestCase):
-    """State"""
+class TestAmenity(unittest.TestCase):
+    """Amenity"""
 
     def test_cls_type(self):
-        self.assertEqual(State, type(State()))
+        self.assertEqual(Amenity, type(Amenity()))
 
     def test_storge_object(self):
-        self.assertIn(State(), models.storage.all().values())
+        self.assertIn(Amenity(), models.storage.all().values())
 
     def test_id_type(self):
-        self.assertEqual(str, type(State().id))
+        self.assertEqual(str, type(Amenity().id))
 
     def test_created_at_type(self):
-        self.assertEqual(datetime, type(State().created_at))
+        self.assertEqual(datetime, type(Amenity().created_at))
 
     def test_updated_at_type(self):
-        self.assertEqual(datetime, type(State().updated_at))
+        self.assertEqual(datetime, type(Amenity().updated_at))
 
     def test_name_type(self):
-        self.assertEqual(str, type(State.name))
+        self.assertEqual(str, type(Amenity.name))
 
     def test_args_unused(self):
-        N = State(None)
+        N = Amenity(None)
         self.assertNotIn(None, N.__dict__.values())
 
     def test_intTypeError(self):
         with self.assertRaises(TypeError):
-            State(id=None, created_at=None, updated_at=None)
+            Amenity(id=None, created_at=None, updated_at=None)
 
     def test_no_args(self):
-        M = State()
-        self.assertIsInstance(M, State)
+        M = Amenity()
+        self.assertIsInstance(M, Amenity)
 
     def test_save_arg(self):
-        N = State()
+        N = Amenity()
         with self.assertRaises(TypeError):
             N.save(None)
 
     def test_to_dict_1(self):
-        N = State()
+        N = Amenity()
         self.assertNotEqual(N.to_dict(), N.__dict__)
 
     def test_to_dict_2(self):
-        N = State()
+        N = Amenity()
         with self.assertRaises(TypeError):
             N.to_dict(None)
 
     def test_h(self):
-        M = State()
+        M = Amenity()
         self.assertEqual(M.name, "")
 
     def test_name_attr(self):
-        M = State()
+        M = Amenity()
         self.assertIn("name", dir(M))
 
     def test_k(self):
-        M = State()
+        M = Amenity()
         self.assertNotIn("name", M.__dict__)
 
     def test_ids(self):
-        N1 = State()
-        N2 = State()
+        N1 = Amenity()
+        N2 = Amenity()
         self.assertNotEqual(N1.id, N2.id)
 
     def test_attributes_assignment(self):
-        N = State()
+        N = Amenity()
         N.name = "Khartum"
         self.assertEqual(N.name, "Khartum")
 
     def test_2_created_at(self):
-        N1 = State()
-        N2 = State()
+        N1 = Amenity()
+        N2 = Amenity()
         self.assertLess(N1.created_at, N2.created_at)
 
     def test_2_updated_at(self):
-        N1 = State()
-        N2 = State()
+        N1 = Amenity()
+        N2 = Amenity()
         self.assertLess(N1.updated_at, N2.updated_at)
 
     def test_str_rep(self):
         """ sec"""
         D = datetime.today()
         D_repr = repr(D)
-        N = State()
+        N = Amenity()
         N.id = "12344"
         N.created_at = N.updated_at = D
         ST = N.__str__()
-        self.assertIn("[State] (12344)", ST)
+        self.assertIn("[Amenity] (12344)", ST)
         self.assertIn("'id': '12344'", ST)
         self.assertIn("'created_at': " + D_repr, ST)
         self.assertIn("'updated_at': " + D_repr, ST)
@@ -106,14 +106,14 @@ class TestState(unittest.TestCase):
         """ sec"""
         D = datetime.today()
         D_IOS = D.isoformat()
-        N = State(id="3333", created_at=D_IOS, updated_at=D_IOS)
+        N = Amenity(id="3333", created_at=D_IOS, updated_at=D_IOS)
         self.assertEqual(N.id, "3333")
         self.assertEqual(N.created_at, D)
         self.assertEqual(N.updated_at, D)
 
     def test_save(self):
         """ sec"""
-        N = State()
+        N = Amenity()
         F = N.updated_at
         N.save()
         L = N.updated_at
