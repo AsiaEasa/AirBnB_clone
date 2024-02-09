0c9d6b60da669bdd6f9529a329da2934ba1711dd
AsiaEasa <asiaeasa89@gmail.com>
2024-02-09 13:37:31 -0800

    done

---
+++ tests/test_console.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines unittests for console.py"""
+
+import os
+import sys
+import unittest
+from models import storage
+from models.engine.file_storage import FileStorage
+from console import HBNBCommand
+from io import StringIO
+from unittest.mock import patch
+
+class TestHBNBCommand_create(unittest.TestCase):
+    """create"""
+
+    def test_mis_class(self):
+        X = "** class name missing **"
+        with patch("sys.stdout", new=StringIO()) as FF:
+            self.assertFalse(HBNBCommand().onecmd("create"))
+            self.assertEqual(X, FF.getvalue().strip())
