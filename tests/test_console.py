bd59efab13212687ba6e1e342eb89b83678aeda9
AsiaEasa <asiaeasa89@gmail.com>
2024-02-10 11:54:39 -0800

    done

--- tests/test_console.py
+++ tests/test_console.py
@@ -9,28 +9,20 @@ from console import HBNBCommand
 from io import StringIO
 from unittest.mock import patch
 
-
 class TestHBNBCommand_create(unittest.TestCase):
+    """create"""
+
+    def test_mis_1(self):
+        X = "** class doesn't exist **"
+        items = ["all", "count"]
+        for item in items:
+            with patch("sys.stdout", new=StringIO()) as FF:
+                self.assertFalse(HBNBCommand().onecmd(f"kkk.{item}()"))
+                self.assertEqual(X, FF.getvalue().strip())
 
     def test_crmissing_cla(self):
         MSG = "** class name missing **"
         CO = ["create"]
-        for C in CO:
-            with patch("sys.stdout", new=StringIO()) as OUT:
-                self.assertFalse(HBNBCommand().onecmd(C))
-                self.assertEqual(MSG, OUT.getvalue().strip())
-
-    def test_smissing_cla(self):
-        MSG = "** class name missing **"
-        CO = ["show", ".show()", ]
-        for C in CO:
-            with patch("sys.stdout", new=StringIO()) as OUT:
-                self.assertFalse(HBNBCommand().onecmd(C))
-                self.assertEqual(MSG, OUT.getvalue().strip())
-
-    def test_desmissing_cla(self):
-        MSG = "** class name missing **"
-        CO = ["destroy", ".destroy()"]
         for C in CO:
             with patch("sys.stdout", new=StringIO()) as OUT:
                 self.assertFalse(HBNBCommand().onecmd(C))
