6d96cb73a3636951fe7ba60a2dacf5a5f16c64a3
Amedamin <ahmedqryqr@gmail.com>
2024-02-10 13:33:31 -0800

    done

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,21 +1,38 @@
 #!/usr/bin/python3
-"""Defines unittests for console.py"""
-
+import os
+import sys
 import unittest
+from models import storage
+from models.engine.file_storage import FileStorage
 from console import HBNBCommand
 from io import StringIO
 from unittest.mock import patch
 
+class TestHBNB(unittest.TestCase):
+    """Unittests for testing prompting of the HBNB command interpreter."""
 
-class TestHBNBCommand_create(unittest.TestCase):
-    """create"""
+    def test_smissing_cla(self):
+        MSG = "** class name missing **"
+        CO = ["show"]
+        for C in CO:
+            with patch("sys.stdout", new=StringIO()) as OUT:
+                self.assertFalse(HBNBCommand().onecmd(C))
+                self.assertEqual(MSG, OUT.getvalue().strip())
 
-    def test_mis(self):
-        X = "** class name missing **"
-        with patch("sys.stdout", new=StringIO()) as FF:
-            self.assertFalse(HBNBCommand().onecmd("create"))
-            self.assertEqual(X, FF.getvalue().strip())
-
+    def test_crmissing_cla(self):
+        MSG = "** class name missing **"
+        CO = ["create"]
+        for C in CO:
+            with patch("sys.stdout", new=StringIO()) as OUT:
+                self.assertFalse(HBNBCommand().onecmd(C))
+                self.assertEqual(MSG, OUT.getvalue().strip())
+    def test_desmissing_cla(self):
+        MSG = "** class name missing **"
+        CO = ["destroy"]
+        for C in CO:
+            with patch("sys.stdout", new=StringIO()) as OUT:
+                self.assertFalse(HBNBCommand().onecmd(C))
+                self.assertEqual(MSG, OUT.getvalue().strip())
     def test_mis_1(self):
         X = "** class doesn't exist **"
         items = ["all", "count"]
@@ -30,21 +47,23 @@ class TestHBNBCommand_create(unittest.TestCase):
         for item in items:
             with patch("sys.stdout", new=StringIO()) as FF:
                 self.assertFalse(HBNBCommand().onecmd(f"{item} kkk"))
+                self.assertEqual(X, FF.getvalue().strip())  
+
+    def test_invalid_1(self):
+        items = ["create"]
+        for item in items:
+            X = f"*** Unknown syntax: kkk.{item}()"
+            with patch("sys.stdout", new=StringIO()) as FF:
+                self.assertFalse(HBNBCommand().onecmd(f"kkk.{item}()"))
                 self.assertEqual(X, FF.getvalue().strip())
 
-    def test_emptyline(self):
-        with patch("sys.stdout", new=StringIO()) as FF:
-            self.assertFalse(HBNBCommand().onecmd(""))
-            self.assertEqual("", FF.getvalue().strip())
-
-    def test_EOF_quit(self):
-        items = ["EOF", "quit"]
+    def test_mis_all(self):
+        X = "** class doesn't exist **"
+        items = ["count", "all"]
         for item in items:
             with patch("sys.stdout", new=StringIO()) as FF:
-                self.assertTrue(HBNBCommand().onecmd(f"{item}"))
+                self.assertFalse(HBNBCommand().onecmd(f"MyModel.{item}()"))
+                self.assertEqual(X, FF.getvalue().strip())
 
-    def test_help_quit(self):
-        X = "Quit command to exit the program."
-        with patch("sys.stdout", new=StringIO()) as FF:
-            self.assertFalse(HBNBCommand().onecmd("help quit"))
-            self.assertEqual(X, FF.getvalue().strip())
+if __name__ == "__main__":
+    unittest.main()
