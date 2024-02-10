a50a0f519585cc98cc136dc6427b99c8cda0788b
AsiaEasa <asiaeasa89@gmail.com>
2024-02-10 11:46:43 -0800

    done

--- tests/test_console.py
+++ tests/test_console.py
@@ -11,39 +11,6 @@ from unittest.mock import patch
 
 
 class TestHBNBCommand_create(unittest.TestCase):
-    """create"""
-
-    def test_mis_1(self):
-        X = "** class doesn't exist **"
-        items = ["all", "count"]
-        for item in items:
-            with patch("sys.stdout", new=StringIO()) as FF:
-                self.assertFalse(HBNBCommand().onecmd(f"kkk.{item}()"))
-                self.assertEqual(X, FF.getvalue().strip())
-
-    def test_mis_1(self):
-        X = "** class doesn't exist **"
-        items = ["show", "destroy"]
-        for item in items:
-            with patch("sys.stdout", new=StringIO()) as FF:
-                self.assertFalse(HBNBCommand().onecmd(f"kkk.{item}()"))
-                self.assertEqual(X, FF.getvalue().strip())
-
-    def test_invalid_1(self):
-        items = ["create"]
-        for item in items:
-            X = f"*** Unknown syntax: kkk.{item}()"
-            with patch("sys.stdout", new=StringIO()) as FF:
-                self.assertFalse(HBNBCommand().onecmd(f"kkk.{item}()"))
-                self.assertEqual(X, FF.getvalue().strip())
-
-    def test_mis_2(self):
-        X = "** class doesn't exist **"
-        items = ["all", "count", "destroy", "show", "update", "create"]
-        for item in items:
-            with patch("sys.stdout", new=StringIO()) as FF:
-                self.assertFalse(HBNBCommand().onecmd(f"{item} kkk"))
-                self.assertEqual(X, FF.getvalue().strip())
 
     def test_crmissing_cla(self):
         MSG = "** class name missing **"
