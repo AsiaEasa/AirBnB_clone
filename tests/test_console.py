21710d9472b5e9f0efc1a6fee770d4ae3f13e64a
AsiaEasa <asiaeasa89@gmail.com>
2024-02-10 12:00:53 -0800

    donw

--- tests/test_console.py
+++ tests/test_console.py
@@ -9,8 +9,15 @@ from console import HBNBCommand
 from io import StringIO
 from unittest.mock import patch
 
+
 class TestHBNBCommand_create(unittest.TestCase):
     """create"""
+
+    def test_mis_class(self):
+        X = "** class name missing **"
+        with patch("sys.stdout", new=StringIO()) as FF:
+            self.assertFalse(HBNBCommand().onecmd("create"))
+            self.assertEqual(X, FF.getvalue().strip())
 
     def test_mis_1(self):
         X = "** class doesn't exist **"
@@ -20,11 +27,11 @@ class TestHBNBCommand_create(unittest.TestCase):
                 self.assertFalse(HBNBCommand().onecmd(f"kkk.{item}()"))
                 self.assertEqual(X, FF.getvalue().strip())
 
-    def test_crmissing_cla(self):
-        MSG = "** class name missing **"
-        CO = ["create"]
-        for C in CO:
-            with patch("sys.stdout", new=StringIO()) as OUT:
-                self.assertFalse(HBNBCommand().onecmd(C))
-                self.assertEqual(MSG, OUT.getvalue().strip())
+    def test_mis_2(self):
+        X = "** class doesn't exist **"
+        items = ["all", "count", "destroy", "show", "update"]
+        for item in items:
+            with patch("sys.stdout", new=StringIO()) as FF:
+                self.assertFalse(HBNBCommand().onecmd(f"{item} kkk"))
+                self.assertEqual(X, FF.getvalue().strip())
 
